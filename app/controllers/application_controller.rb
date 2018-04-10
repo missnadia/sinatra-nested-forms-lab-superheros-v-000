53ae0568aca45fa7ec4348413875718dc33b36ff
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-10 23:55:15 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,17 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :team
+    end
 
+    post '/teams' do
+      @team = Team.new(params[:team])
+
+      params[:team][:heroes].each { |details| SuperHero.new(details) }
+
+      @heroes = SuperHero.all
+
+      erb :super_hero
+    end
 end
